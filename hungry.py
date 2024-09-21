600de783ba6330f654f554b334c97bfe9120a1fd
Nouman022 <nouman02022@gmail.com>
2024-09-22 03:40:05 +0500

    Eat_Burger

--- hungry.py
+++ hungry.py
@@ -2,7 +2,7 @@ hungry = input("Are you hungry? : ")
 if hungry == "yes":
     print("Eat Samosa")
     print("Eat Pizza")
-    print("Eat Burger")
+    print("Eat_Burger")
 else:
     thirsty = input("Are you thirsty? : ")
     if thirsty == "yes":
