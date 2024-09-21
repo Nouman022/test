f72f15f42428a52313f9b38099a0265659bb5508
Nouman022 <nouman02022@gmail.com>
2024-09-22 04:20:25 +0500

    remove extras

--- hungry.py
+++ hungry.py
@@ -1,10 +1,7 @@
 hungry = input("Are you hungry? : ")
 if hungry == "yes":
     print("Eat Samosa")
-    print("Eat Pizza")
-    print("Eat_Burger")
 else:
     thirsty = input("Are you thirsty? : ")
     if thirsty == "yes":
         print("Drink Water")
-        print("Drink Soda")
